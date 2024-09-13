991c459178a1239c5fb89a8b811a169ad89b0b40
prasanna <prasannahegde245@gmail.com>
2024-09-13 15:52:40 +0530

    added services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,NavbarComponent,SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None 
 })
 export class AppComponent {
   title = 'cmd-ui-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ViewDoctorComponent } from './view-doctor/view-doctor.component';
+import { AddDoctorComponent } from './add-doctor/add-doctor.component';
+import { EditDoctorComponent } from './edit-doctor/edit-doctor.component';
+import { ViewDoctorScheduleComponent } from './view-doctor-schedule/view-doctor-schedule.component';
+import { EditDoctorScheduleComponent } from './edit-doctor-schedule/edit-doctor-schedule.component';
+import { AddDoctorScheduleComponent } from './add-doctor-schedule/add-doctor-schedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'doctor',component:ViewDoctorComponent,title:'doctors'},
+    {path:'doctor/add',component:AddDoctorComponent,title:'add doctor'},
+    {path:'doctor/edit',component:EditDoctorComponent,title:'edit doctor'},
+    {path:'view-schedule',component:ViewDoctorScheduleComponent,title:'doctor schedule'},
+    {path:'view-schedule/edit',component:EditDoctorScheduleComponent,title:'edit schedule'},
+    {path:'view-schedule/add',component:AddDoctorScheduleComponent,title:'add schedule'},
+];
