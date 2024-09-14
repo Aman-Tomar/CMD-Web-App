19f260fff76cc0d6e2c00645b339e2d252a7ebda
Srusti MH <srusti.mh@consilio.com>
2024-09-14 18:14:19 +0530

    patient details added

--- src/app/patient_feature/patient-detail/patient-detail.component.ts
+++ src/app/patient_feature/patient-detail/patient-detail.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Patient } from '../../models/patient.model';
+import { PatientService } from '../../services/patient/patient.service';
 
 @Component({
   selector: 'app-patient-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './patient-detail.component.html',
   styleUrl: './patient-detail.component.css'
 })
-export class PatientDetailComponent {
+export class PatientDetailComponent 
+{
+  patient: Patient | null = null;
 
+  constructor(private route : ActivatedRoute, private patientService : PatientService){}
+
+  ngOnInit(): void {
+    const patientId = this.route.snapshot.paramMap.get('id');
+    if (patientId) {
+      this.loadPatient(+patientId);
+    }
+  }
+
+  loadPatient(id: number): void {
+    this.patientService.getPatientById(id).subscribe(
+      {
+        next: (patient) => {
+          this.patient = patient;
+        },
+        error: (error) => {
+          console.error('Error loading patient:', error);
+        }
+      } 
+    )
+  }
 }
