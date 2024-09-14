a1c25546083ee58a6f100e73b1f10d02870c6640
Ritik Anubhav <Ritik.Anubhav@consilio.com>
2024-09-14 14:15:57 +0530

    add-appointment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AppointmentListComponent } from './appointment_feature/appointment-list/appointment-list.component';
+import { AddAppointmentComponent } from './appointment_feature/add-appointment/add-appointment.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
-    {path:'appointments',component:AppointmentListComponent,title:"All appointments"}
+    {path:'appointments',component:AppointmentListComponent,title:"All appointments"},
+    {path:'appointments/add',component:AddAppointmentComponent,title:"Add appointment"}
 ];
