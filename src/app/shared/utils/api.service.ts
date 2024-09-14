b95daf71744fd7b5835606d20aaa6ca4caae77ed
Megha <Megha@consilio.com>
2024-09-14 11:42:40 +0530

    Added basic structure for patient module

---
+++ src/app/shared/utils/api.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  //private PATIENT_BASE_URL = "https://cmdpatientnewwebapp-ckbwb7h0cjhrfehx.southeastasia-01.azurewebsites.net/api/patients";
+  constructor() { }
+}
