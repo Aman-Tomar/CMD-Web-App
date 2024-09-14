951223a2e12e30abe75efced2475eb9681be088f
Srusti MH <srusti.mh@consilio.com>
2024-09-14 17:52:43 +0530

    pulled from feature app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,16 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NgClass } from '@angular/common';
 import { SidebarService } from './sidebar.service';
+import { PatientListComponent } from './patient_feature/patient-list/patient-list.component';
+import { PatientDetailComponent } from './patient_feature/patient-detail/patient-detail.component';
+import { PatientFormComponent } from './patient_feature/patient-form/patient-form.component';
+import { PatientCardComponent } from './patient_feature/patient-card/patient-card.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,SidebarComponent,NgClass],
+  imports: [NgClass,RouterOutlet,NavbarComponent,SidebarComponent,PatientListComponent,PatientDetailComponent, PatientFormComponent, PatientCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/patient_feature/patient-card/patient-card.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Patient } from '../../models/patient.model';
+
+@Component({
+  selector: 'app-patient-card',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './patient-card.component.html',
+  styleUrl: './patient-card.component.css'
+})
+export class PatientCardComponent {
+  @Input() patient! : Patient;
+
+  get avatarUrl(): string {
+    return typeof this.patient.image === 'string' ? this.patient.image : 'assets/img/default-avatar.png';
+  }
+}
