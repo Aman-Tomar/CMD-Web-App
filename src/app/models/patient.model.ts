46a5cdd3e67ac6105df48234546e8f89c5d04c37
Megha <Megha@consilio.com>
2024-09-14 11:42:40 +0530

    Added basic structure for patient module

---
+++ src/app/models/patient.model.ts
@@ -0,0 +1,35 @@
+export interface Patient {
+    patientId?: number;
+    patientName: string;
+    email: string;
+    phone: string;
+    age: number;
+    dob: string;
+    gender: string;
+    preferredStartTime: string | null;
+    preferredEndTime: string | null;
+    createdDate: string;
+    createdBy: number;
+    lastModifiedDate: string;
+    lastModifiedBy: number;
+    preferredClinicId: number | null;
+    image?: File |string | null;
+    patientAddressId: number;
+    streetAddress: string;
+    city: string;
+    state: string;
+    country: string;
+    zipCode: string;
+    preferredDoctorId: number | null;
+    patientGuardianId: number | null;
+    patientGuardianName: string | null;
+    patientGuardianPhoneNumber: string | null;
+    patientGuardianRelationship: string | null;
+}
+
+export interface PatientResponse {
+    items: Patient[];
+    totalCountOfPatients: number;
+    pageNumber: number;
+    pageSize: number;
+}
---
+++ src/app/services/patient/patient.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PatientResponse, Patient } from '../../models/patient.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+
+  private PATIENT_BASE_URL = "https://cmdpatientnewwebapp-ckbwb7h0cjhrfehx.southeastasia-01.azurewebsites.net/api/patients";
+  
+  constructor(private http: HttpClient) { }
+
+  getPatients(pageNumber: number, pageSize: number): Observable<PatientResponse> {
+    const params = new HttpParams()
+      .set('pageNumber', pageNumber.toString())
+      .set('pageSize', pageSize.toString());
+    return this.http.get<PatientResponse>(`${this.PATIENT_BASE_URL}`, { params });
+  }
+
+  getPatientById(patientId: number): Observable<Patient> {
+    return this.http.get<Patient>(`${this.PATIENT_BASE_URL}/${patientId}`);
+  }
+
+  createPatient(patient: Patient): Observable<Patient> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.http.post<Patient>(`${this.PATIENT_BASE_URL}`, patient, { headers });
+  }
+
+  updatePatient(patientId: number, patient: Patient): Observable<Patient> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.http.put<Patient>(`${this.PATIENT_BASE_URL}/${patientId}`, patient, { headers });
+  }
+  
+}
