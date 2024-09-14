e38ed8760a333513c284bebb4f76fab491782466
Ritik Anubhav <Ritik.Anubhav@consilio.com>
2024-09-15 00:22:37 +0530

    status bug solved

--- src/app/appointment_feature/appointment-details/appointment-details.component.ts
+++ src/app/appointment_feature/appointment-details/appointment-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { AppointmentService } from '../../services/appointmnent/appointment.service';
 import { IAppointment } from '../../models/Appointment/Appointment';
 import { NgClass } from '@angular/common';
+import { AppointmentStatus } from '../../models/Appointment/AppointmentStatus';
 
 @Component({
   selector: 'app-appointment-details',
@@ -12,6 +13,7 @@ import { NgClass } from '@angular/common';
   styleUrl: './appointment-details.component.css'
 })
 export class AppointmentDetailsComponent {
+  AppointmentStatus = AppointmentStatus;
   route:ActivatedRoute=inject(ActivatedRoute);
   service:AppointmentService=inject(AppointmentService);
   router:Router=inject(Router);
