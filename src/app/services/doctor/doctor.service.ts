6bd250138762258a9e9d2662cee297e02a3527f7
prasanna <prasannahegde245@gmail.com>
2024-09-16 01:47:31 +0530

    added necessary fields in the form

--- src/app/services/doctor/doctor.service.ts
+++ src/app/services/doctor/doctor.service.ts
@@ -10,6 +10,7 @@ import { environment } from '../../../environments/environment';
 })
 export class DoctorService {
   private apiUrl = environment.doctorBaseUrl; 
+  private clinicUrl = environment.clinicBaseUrl;
   client:HttpClient = inject(HttpClient);
 
   getAllDoctors(page: number, pageSize: number): Observable<any> {
@@ -34,6 +35,18 @@ export class DoctorService {
     return this.client.put<any>(`${this.apiUrl}/Doctor?doctorId=${doctorId}`, formData);
   }
   
+  getSpecializations(): Observable<string[]> {
+    return this.client.get<string[]>(`http://localhost:3000/specializations`);
+  }
+
+  getDepartments(): Observable<any[]> {
+    return this.client.get<any[]>(`${this.clinicUrl}/api/Department`);
+  }
+
+  getClinics(): Observable<any[]> {
+    return this.client.get<any[]>(`${this.clinicUrl}/api/Clinic`);
+  }
+
   private mapDoctorResponse(response: any): IDoctor {
     return {
       doctorId: response.doctorId,
