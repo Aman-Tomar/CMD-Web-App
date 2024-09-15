c596d5bce542a37a13820477d075c2dff3fc64e6
Aryan <singh.aryan837@gmail.com>
2024-09-15 17:38:53 +0530

    deleted status code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AppointmentListComponent } from './appointment_feature/appointment-list/appointment-list.component';
 import { AddAppointmentComponent } from './appointment_feature/add-appointment/add-appointment.component';
-import { AppointmentFilterComponent } from './appointment_feature/appointment-filter-status/appointment-filter-status.component';
 import { EditAppointmentComponent } from './appointment_feature/edit-appointment/edit-appointment.component';
 import { AppointmentDetailsComponent } from './appointment_feature/appointment-details/appointment-details.component';
 
@@ -10,7 +9,6 @@ export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path:'appointments',component:AppointmentListComponent,title:"All appointments"},
     {path:'appointments/add',component:AddAppointmentComponent,title:"Add appointment"},
-    {path:'appointments/filterbystatus',component:AppointmentFilterComponent,title:"Filter By Status"},
     {path:'appointments/:id',component:AppointmentDetailsComponent,title:"View Appointment"},
     {path:'appointments/edit/:id', component:EditAppointmentComponent,title:"Edit Appointment" },
 ];
