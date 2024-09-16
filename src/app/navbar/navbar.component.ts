92012c7b11f95a15279d4d2df3a00e1c9186a784
Aryan <singh.aryan837@gmail.com>
2024-09-16 22:42:29 +0530

    Added dashboard data

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,31 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SidebarService } from '../sidebar.service';
-import { AuthserviceService } from '../services/auth/authservice.service';
-import { TokenService } from '../services/token/token.service';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgIf],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements OnInit {
-  constructor(private sidebarService: SidebarService,private authService: AuthserviceService,private tokenService:TokenService) {}
-  loggedIn=false;
-  ngOnInit(): void {
-    this.checkLoggedIn();
-  }
-
-  checkLoggedIn()
-  {
-    const token=this.tokenService.getToken();
-    if(token)
-    {
-      this.loggedIn=true;
-    }
-  }
+export class NavbarComponent {
+  constructor(private sidebarService: SidebarService) {}
 
   toggleSidebar() {
     this.sidebarService.toggleSidebar();
